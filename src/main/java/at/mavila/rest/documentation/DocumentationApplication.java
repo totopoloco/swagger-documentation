99f27157668a3907f5e7297ea205d34c285bfbd0
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2024-05-13 14:24:18 +0200

    none initial commit

---
+++ src/main/java/at/mavila/rest/documentation/DocumentationApplication.java
@@ -0,0 +1,13 @@
+package at.mavila.rest.documentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/mavila/rest/documentation/DocumentationApplicationTests.java
@@ -0,0 +1,13 @@
+package at.mavila.rest.documentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
