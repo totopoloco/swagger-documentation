4d0caa9899378cc8faeb3c09800f727e797b55e9
Marco Tulio Ávila Cerón <marcot@avilac.eu>
2024-05-20 04:22:56 +0200

    none Renames endpoints and demos methods

--- src/main/java/at/mavila/rest/documentation/CountryController.java
+++ src/main/java/at/mavila/rest/documentation/CountryController.java
@@ -1,32 +1,49 @@
 package at.mavila.rest.documentation;
 
-import at.mavila.rest.documentation.api.CountryFetchApi;
+import at.mavila.rest.documentation.api.CountryApi;
+import at.mavila.rest.documentation.model.AddCountryRequest;
 import at.mavila.rest.documentation.model.Country;
+import at.mavila.rest.documentation.model.CountryResponse;
 import at.mavila.rest.documentation.model.Language;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @Slf4j
-public class CountryController implements CountryFetchApi {
-  @Override
-  public ResponseEntity<List<Country>> fetchAllCountries() {
+public class CountryController implements CountryApi {
 
-    List<Country> countries = new ArrayList<>();
-    Country austria = new Country();
-    austria.setEnglishName(Optional.of("Austria"));
-    austria.setLanguages(List.of(new Language("German")));
-    Country mexico = new Country();
-    mexico.setEnglishName(Optional.of("Mexico"));
-    mexico.setLanguages(List.of(new Language("Spanish")));
+    @Override
+    public ResponseEntity<CountryResponse> addCountry(Optional<AddCountryRequest> addCountryRequest) {
+        return addCountryRequest.map(request -> {
+            log.info("Adding country with name: {}", request.getEnglishName());
 
-    countries.add(austria);
-    countries.add(mexico);
+            CountryResponse response = new CountryResponse();
+            response.setEnglishName(request.getEnglishName());
+            response.setId(Optional.of(1));
 
-    return ResponseEntity.ok(countries);
-  }
+            return ResponseEntity.ok(response);
+        }).orElseGet(() -> ResponseEntity.badRequest().body(null));
+    }
+
+    @Override
+    public ResponseEntity<List<Country>> fetchAllCountries() {
+
+        List<Country> countries = new ArrayList<>();
+        Country austria = new Country();
+        austria.setEnglishName(Optional.of("Austria"));
+        austria.setLanguages(List.of(new Language("German")));
+        Country mexico = new Country();
+        mexico.setEnglishName(Optional.of("Mexico"));
+        mexico.setLanguages(List.of(new Language("Spanish")));
+
+        countries.add(austria);
+        countries.add(mexico);
+
+        return ResponseEntity.ok(countries);
+    }
 }
--- src/main/java/at/mavila/rest/documentation/LanguageController.java
+++ src/main/java/at/mavila/rest/documentation/LanguageController.java
@@ -1,14 +1,15 @@
 package at.mavila.rest.documentation;
 
-import at.mavila.rest.documentation.api.LanguageFetchApi;
+import at.mavila.rest.documentation.api.LanguageApi;
 import at.mavila.rest.documentation.model.Language;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
-public class LanguageController implements LanguageFetchApi {
+public class LanguageController implements LanguageApi {
   @Override
   public ResponseEntity<Language> addLanguage(Optional<Language> language) {
     return ResponseEntity.of(language);
